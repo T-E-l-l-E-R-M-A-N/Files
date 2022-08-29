d7c6b134ac1b2f572aee3257890f8d7c67d1ea91
dmitrij-471 <dboycov.2005@gmail.com>
2022-08-29 10:59:06 +0300

    Create Files.UI and Core Projects

---
+++ src/Files/UI/Files.UI/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using MarbleUI.Controls;
+
+namespace Files.UI
+{
+    public partial class MainWindow : UIWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
